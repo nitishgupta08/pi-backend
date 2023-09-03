bec0267fe692509ebb9cea68649f5aee59077c64
nitishgupta08 <nitishkg.88@gmail.com>
2023-09-04 02:41:14 +0530

    clean up

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("login/", TokenObtainPairView.as_view(), name="login_obtain_pair"),
     path("login/refresh/", TokenRefreshView.as_view(), name="login_refresh"),
     path("register/", RegisterUserView.as_view(), name="register_user"),
-    path("profile/<str:username>/", ProfileView.as_view(), name="user_profile"),
+    path("profile/<str:username>", ProfileView.as_view(), name="user_profile"),
 ]
